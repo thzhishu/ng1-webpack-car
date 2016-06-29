c1393c3bf6018fbf576ecbc84e851cf62fbc2f56
Rain.xia <rain.xia@mytianhui.com>
2016-06-29 18:28:14 +0800

    menus

--- client/app/common/common.js
+++ client/app/common/common.js
@@ -1,12 +1,12 @@
 import angular from 'angular';
 import Navbar from './navbar/navbar';
 import Menus from './menus/menus';
-import Hero from './hero/hero';
+//import Hero from './hero/hero';
 import User from './user/user';
 
 let commonModule = angular.module('app.common', [
   Navbar.name,
-  Hero.name,
+  //Hero.name,
   User.name,
   Menus.name
 ]);
