6378fc8c9626c02a7466faed0e605ee13967e746
ToBePlus <float1101@gmail.com>
2016-07-04 16:42:40 +0800

    添加 主logo 底部声明

--- client/app/common/common.js
+++ client/app/common/common.js
@@ -2,12 +2,12 @@ import angular from 'angular';
 import Navbar from './navbar/navbar';
 import Menus from './menus/menus';
 import PageFooter from './pageFooter/pageFooter';
-//import Hero from './hero/hero';
+import MainLogo from './mainLogo/mainLogo';
 import User from './user/user';
 
 let commonModule = angular.module('app.common', [
   Navbar.name,
-  //Hero.name,
+  MainLogo.name,
   User.name,
   Menus.name,
   PageFooter.name
