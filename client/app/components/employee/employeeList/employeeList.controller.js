843dc9f76b48ace3fcc7fdbe8c6021a66e549df6
ToBePlus <float1101@gmail.com>
2016-07-04 11:56:04 +0800

    添加路由 快捷入口

--- client/app/components/employee/employeeList/employeeList.controller.js
+++ client/app/components/employee/employeeList/employeeList.controller.js
@@ -2,6 +2,12 @@ class EmployeeListController {
   constructor() {
     this.name = 'employeeList';
   }
+  onClose() {
+    this.winShow = 0;
+  }
+  onDel() {
+    this.winShow = 1;
+  }
 }
 
 export default EmployeeListController;
